d703888e83638431874107ec0d7fe2f36a4ba512
mbjaghou <mouhamedbj61@gmail.com>
2023-01-15 18:45:44 +0100

    inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,7 @@
-   service mysql start &&  mysql -e "CREATE DATABASE inception;" && \
-   mysql -e  "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"  && \
-    mysql -e "FLUSH PRIVILEGES;"
-
+    if [ ! -d /var/lib/mysql/inception ]; then
+    service mysql start && mysql -e "CREATE DATABASE inception;"
+   mysql -e  "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
+   mysql -e "GRANT ALL PRIVILEGES ON inception.* TO 'wordpressuser'@'localhost';"
+   mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '0000';"
+     mysql -e "FLUSH PRIVILEGES;"
+    fi
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,8 @@
+
+
+
+
+
+wp core download --allow-root 
+wp config create --dbname=inception --dbuser=wordpressuser --dbpass=password --locale=localhost
+# wp core install --allow-root
