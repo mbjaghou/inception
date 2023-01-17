5c699fc22a9eed15a40f9e5b696f804602ef545b
mbjaghou <93668415+mbjaghou@users.noreply.github.com>
2023-01-17 20:10:45 +0100

    update

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,8 @@
-if [ ! -d /var/lib/mysql/inception ]; then
+if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
+
+then
+    echo "DATABASE EXIST"
+else
     service mysql start && mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
     mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
