fee2d168c3a3d074c955ef2e551d53bcc26893fc
mbjaghou <93668415+mbjaghou@users.noreply.github.com>
2023-01-17 20:55:08 +0100

    Delete script.sh

--- srcs/requirements/nginx/tools/script.sh
+++
@@ -1,4 +0,0 @@
-
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/mbjaghou.key -out /etc/nginx/ssl/mbjaghou.crt -subj "/C=MA/ST=BN/L=Khouribga/O=1337/OU=mbjaghou/CN=localhst"
-
-nginx -g "daemon off;"
