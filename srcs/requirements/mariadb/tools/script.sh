e66e88f21fa1635e7634cbdd74322ad8e9bd8f0a
mbjaghou <93668415+mbjaghou@users.noreply.github.com>
2023-01-17 20:39:17 +0100

    Update script.sh

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,10 +4,10 @@ then
     echo "DATABASE EXIST"
 else
     service mysql start
-    mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -e "FLUSH PRIVILEGES;"
-    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
+    mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -u root -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
     mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
