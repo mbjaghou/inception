51eedf07404bee8561233b75f5aa1c1d42ea6396
mbjaghou <mouhamedbj61@gmail.com>
2023-01-17 18:42:47 +0100

    databases

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,10 +1,10 @@
 if [ ! -d /var/lib/mysql/inception ]; then
-    service mysql start && mysql -e "CREATE DATABASE inception;"
-    mysql -e  "CREATE USER 'wordpressuser'@'%' IDENTIFIED BY 'password';"
-    mysql -e "GRANT ALL PRIVILEGES ON inception.* TO 'wordpressuser'@'%';"
+    service mysql start && mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
-    sleep 2
-    mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '0000';"
-    mysqladmin -uroot -p0000 shutdown
+    sleep 5
+    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
+    mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
 mysqld_safe
--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,5 +1,4 @@
 
-mkdir /etc/nginx/ssl/
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/mbjaghou.key -out /etc/nginx/ssl/mbjaghou.crt -subj "/C=MA/ST=BN/L=Khouribga/O=1337/OU=mbjaghou/CN=localhost"
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/mbjaghou.key -out /etc/nginx/ssl/mbjaghou.crt -subj "/C=MA/ST=BN/L=Khouribga/O=1337/OU=mbjaghou/CN=localhst"
 
 nginx -g "daemon off;"
