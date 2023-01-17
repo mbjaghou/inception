c752d37b2af526857ee6b0e0d47d38cb749368df
mbjaghou <93668415+mbjaghou@users.noreply.github.com>
2023-01-17 20:34:52 +0100

    Update script.sh

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,12 +3,11 @@ if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
 then
     echo "DATABASE EXIST"
 else
-    service mysql start && mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    service mysql start
+    mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
     mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
-    sleep 5
-    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
+    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
     mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
-mysqld_safe
