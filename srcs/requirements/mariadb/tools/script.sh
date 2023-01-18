744aa0ac2ce673cf04cc80459a3d5241f3f4e753
mbjaghou <mouhamedbj61@gmail.com>
2023-01-18 15:28:40 +0100

    inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,15 @@
 if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
-
 then
-    echo "DATABASE EXIST"
+    echo "========DATABASE ALEARDY EXIST==========="
 else
     service mysql start
-    mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    mysql -u root -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -u root -e "FLUSH PRIVILEGES;"
-    mysql -u root -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" ;
+    mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    sleep 5
+    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" ;
     mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
 fi
+exec "$@"
