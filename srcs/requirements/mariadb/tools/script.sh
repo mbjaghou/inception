12dadd68f99d79844215f4f6eb37aac73f23637c
mbjaghou <mouhamedbj61@gmail.com>
2023-01-20 14:50:29 +0100

    inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,12 +2,13 @@ if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
 then
     echo "========DATABASE ALREADY EXIST==========="
 else
-    service mysql start 2>/dev/null;
+    service mysql start;
+    sleep 1;
     mysql -e "CREATE DATABASE ${MYSQL_DATABASE};";
     mysql -e  "CREATE USER  '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';";
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';";
     mysql -e "FLUSH PRIVILEGES;";
-    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
+    mysql -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';";
     mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown;
 fi
 exec "$@"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,8 +2,7 @@ if [ -f /var/www/html/wordpress/wp-config.php ]
 then 
     echo "=====wp-config.php exists!====="
 else
-        # sleep 3
-        # create wp-config file
+        # Generates and reads the wp-config.php file
         wp config create --allow-root --dbname=${MYSQL_DATABASE} \
          --dbuser=${MYSQL_USER} \
          --dbpass=${MYSQL_PASSWORD} \
@@ -11,12 +10,27 @@ else
          --path=${PATH_WP}
 
         #install wordpress and creats an admin user
-        wp core install --url=localhost --title=${TITLE_WP} --admin_user=${ADMIN} --admin_password=${ADMIN_PASS} --admin_email=${ADMIN_EMAIL} --path=${PATH_WP} --allow-root
+        wp core install --url=localhost \
+        --title=${TITLE_WP} \
+        --admin_user=${ADMIN} \
+        --admin_password=${ADMIN_PASS} \
+        --admin_email=${ADMIN_EMAIL} \
+        --path=${PATH_WP} --allow-root
 
-        #create new admin user wordpress
-        wp user create ${USER_WP} ${MAIL_WP}  --user_pass=${USER_PASSWORD_WP} --role=author --path=${PATH_WP} --allow-root
+        #create new user wordpress
+        wp user create ${USER_WP} ${MAIL_WP} \
+        --user_pass=${USER_PASSWORD_WP} \
+        --role=author \
+        --path=${PATH_WP} --allow-root
         #install theme for my website
-        wp theme install rife-free --activate --path=${PATH_WP} --allow-root
+        wp theme install rife-free --activate \
+        --path=${PATH_WP} --allow-root
+
+        #create new comment
+        wp comment create --comment_post_ID=1 \
+        --comment_content="hello i am mouhamed bjaghou" \
+        --comment_author="mouhamed bjaghou" \
+        --path=${PATH_WP} --allow-root
         service php7.3-fmp stop
 fi
 php-fpm7.3 -F
